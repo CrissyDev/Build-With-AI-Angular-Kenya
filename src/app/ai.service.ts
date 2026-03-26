6cc6c42bba3b408dbb0b392b3000221cb4c0a277
CrissyDev <christalriziki@gmail.com>
2026-03-26 09:31:13 +0300

    fixing AI

--- src/app/ai.service.ts
+++ src/app/ai.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
+import { environment } from '../app/enviroments/enviroments';
 
 @Injectable({ providedIn: 'root' })
 export class AiService {
-  apiKey = 'AIzaSyC--psLKvCPDA1xqM9SnlzDRDuLmCEG0eQ';
+
+  apiKey = environment.geminiApiKey;
 
   async getReminder(tasks: any[]) {
     const prompt = `Here are my tasks: ${JSON.stringify(tasks)}.
 Suggest a helpful reminder for me.`;
 
     const res = await axios.post(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${this.apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${this.apiKey}`,
       {
         contents: [{ parts: [{ text: prompt }] }]
       }
---
+++ src/app/enviroments/enviroments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    geminiApiKey: 'AIzaSyC--psLKvCPDA1xqM9SnlzDRDuLmCEG0eQ'
+  };
